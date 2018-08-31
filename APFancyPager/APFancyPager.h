55f072df5c7226653d48c967d8f249bbb50374d1
Orhun Mert Simsek <mert@apps.com.tr>
2018-08-31 11:49:58 +0300

    Initial Commit

---
+++ APFancyPager/APFancyPager.h
@@ -0,0 +1,19 @@
+//
+//  APFancyPager.h
+//  APFancyPager
+//
+//  Created by Orhun Mert Şimşek on 31.08.2018.
+//  Copyright © 2018 Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APFancyPager.
+FOUNDATION_EXPORT double APFancyPagerVersionNumber;
+
+//! Project version string for APFancyPager.
+FOUNDATION_EXPORT const unsigned char APFancyPagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APFancyPager/PublicHeader.h>
+
+
